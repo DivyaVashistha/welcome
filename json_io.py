c682ecee76addf70b1f6238f8d8d68f61c66b6be
Vikram <vikrammmarkiv@gmail.com>
2018-02-07 15:19:52 +0530

    hello

--- json_io.py
+++ json_io.py
@@ -4,15 +4,17 @@ import sys
 
 from flask import Flask, render_template, request, redirect, Response
 import random, json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/receiver', methods = ['POST'])
 def worker():
 	# read json + reply
 	data = request.get_json()
 	result = 'sampa pagal!!! :['
-
+	
 	return result
 
 if __name__ == '__main__':
